fbd433a9bc991ccf0a831d4adf32eb0fd5b447a6
Cristina Peña <CristinaLPena@gmail.com>
2024-01-21 15:12:22 -0700

    Change div to section for semantic html

--- src/components/SectionLayout/SectionLayout.js
+++ src/components/SectionLayout/SectionLayout.js
@@ -2,13 +2,13 @@ import styles from './SectionLayout.module.scss';
 
 const SectionLayout = ({ header, iconSrc, children, iconAltText }) => {
     return (
-        <div className={styles.SectionLayout}>
+        <section className={styles.SectionLayout}>
             <h2 className={styles.sectionLayoutHeader}>{header}</h2>
             <img src={iconSrc} className={styles.sectionLayoutIcon} alt={iconAltText} />
             <div className={styles.sectionLayoutText}>
                 <div className={styles.sectionLayoutContent}>{children}</div>
             </div>
-        </div>
+        </section>
     );
 }
 
