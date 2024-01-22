fb8b7fc850816b2c0bdab60c993135018fc8807f
Cristina Peña <CristinaLPena@gmail.com>
2024-01-22 16:29:10 -0700

    Update about

--- src/components/AboutMeSection/AboutMeSection.js
+++ src/components/AboutMeSection/AboutMeSection.js
@@ -12,7 +12,7 @@ const AboutMeSection = () => {
         >
             <p className="aboutMeText">
             Hi! I'm a UX Engineer passionate about design systems, technical writing, learning and development,
-            and cross-functional collaboration. I'm driven by empathy to create user-centered and
+            inclusivity, and cross-functional collaboration. I'm driven by empathy to create user-centered and
             accessible component libraries, thorough documentation, and engaging instructional resources.
             Away from my keyboard, you can find me traveling, reading, or snuggling my three dogs.
             </p>
